18e3d0bf4bce500bc7aaaf7890924353e6ea3333
indsf <northfce@naver.com>
2025-10-12 17:57:01 +0900

    d

--- demo/src/main/java/com/test/member/mapper/MemberMapper.java
+++ demo/src/main/java/com/test/member/mapper/MemberMapper.java
@@ -1,14 +1,4 @@
-<<<<<<< HEAD:demo/src/main/java/com/test/member/mapper/MemberMapper.java
 package com.test.member.mapper;
-=======
-package com.test.Member.mapper;
-
-import com.test.Member.dto.JoinDto;
-import com.test.Member.entity.Member;
-import com.test.Member.entity.MemberRole;
-import com.test.Member.entity.Role;
-import com.test.Member.repository.MemberRepository;
->>>>>>> develop:demo/src/main/java/com/test/Member/mapper/MemberMapper.java
 
 import com.test.member.dto.JoinDto;
 import com.test.member.entity.Member;
@@ -29,7 +19,6 @@ public class MemberMapper {
                 .age(dto.getAge())
                 .disabilityType(dto.getDisabilityType())
                 .memberRole(dto.getMemberRole())
-                .role(dto.getRole())
                 .build();
     }
 
